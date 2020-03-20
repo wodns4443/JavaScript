8b754b4726588cf05afbd1c9b44d5bb1f443479c
wodns4443 <wodns4443@naver.com>
2020-03-20 16:26:05 +0900

    script 1일차

---
+++ test.js
@@ -0,0 +1,7 @@
+
+	var num = 0;
+	console.log('aaaaaaaaaaaaaaa')
+	document.write("head 태그 내 실행 순서 : " +num+"<br>")
+	var age = 23
+	document.write("<div style='color:red; font-size:24px;'>"
+						+"자바 스크립</div>")
